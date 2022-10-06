9a883c27622cfdcbac3c3b6dd48092213f949055
quepas <kiepas.patryk@gmail.com>
2022-10-06 21:03:49 +0200

    Adding working drawing with SDL

--- src/main.c
+++ src/main.c
@@ -1,3 +1,27 @@
-int main(int argc, char** argv) {
+#include <SDL2/SDL.h>
+
+int main(int argc, char **argv) {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow(
+            "ray_tracer",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            640,
+            480,
+            0
+    );
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderDrawPoint(renderer, 320, 220);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
